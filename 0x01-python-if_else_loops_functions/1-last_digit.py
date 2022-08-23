24cdee8de8fe49621058581eabb3c3da2e4b85ef
cosmic <kingsleyfrancis42@gmail.com>
2022-08-23 17:21:32 -0400

    sometimes a bird at hand is better than 10 in the bush except that bush is in a zoo then the latter is bette -lost

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,7 +7,7 @@ if number >= 0:
     if remainder > 5:
         print(f'Last digit of {number} is {remainder} and is greater than 5')
     elif remainder == 0:
-        print(f'Last digit of {number} is {remainder} and is')
+        print(f'Last digit of {number} is {remainder} and is 0')
     elif remainder < 6 and not 0:
         print(f'Last digit of {number} is {remainder}', end=" ")
         print('and is less than 6 and not 0')
@@ -16,7 +16,8 @@ elif number < 0:
     if remainder > 5:
         print(f'Last digit of {number} is {remainder} and is greater than 5')
     elif remainder == 0:
-        print(f'Last digit of {number} is {remainder} and is')
+        print(f'Last digit of {number} is {remainder} and is 0')
     elif remainder < 6 and not 0:
         print(f'Last digit of {number} is {remainder}', end=" ")
         print('and is less than 6 and not 0')
+# i think there is a little bug somewher and  just needs a quick fix
