83ad6a193b244b8178e3b8febc18f0670d9bfbc4
cosmic <kingsleyfrancis42@gmail.com>
2022-08-23 16:37:27 -0400

    i learne to do python commit i have installed pycodestyle by myself

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+if number > 0:
+    print("number is positive")
+elif number < 0:
+    print("numbe is negative")
