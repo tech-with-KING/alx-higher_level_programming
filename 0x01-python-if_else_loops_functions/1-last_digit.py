374f2f4b05351445a46ad676aa1af3fc84e8f942
cosmic <kingsleyfrancis42@gmail.com>
2022-08-23 17:17:48 -0400

    sometimes a bird at hand is better than 10 in the bush except that bush is in a zoo then the latter is bette -lost

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+if number >= 0:
+    remainder = number % 10
+    if remainder > 5:
+        print(f'Last digit of {number} is {remainder} and is greater than 5')
+    elif remainder == 0:
+        print(f'Last digit of {number} is {remainder} and is')
+    elif remainder < 6 and not 0:
+        print(f'Last digit of {number} is {remainder}', end=" ")
+        print('and is less than 6 and not 0')
+elif number < 0:
+    remainder = 10 - (number % 10)
+    if remainder > 5:
+        print(f'Last digit of {number} is {remainder} and is greater than 5')
+    elif remainder == 0:
+        print(f'Last digit of {number} is {remainder} and is')
+    elif remainder < 6 and not 0:
+        print(f'Last digit of {number} is {remainder}', end=" ")
+        print('and is less than 6 and not 0')
