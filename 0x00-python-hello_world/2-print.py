17fdc6c71a0bfc4217c9a67cb384fe8e62989c2b
cosmic <kingsleyfrancis42@gmail.com>
2022-08-24 04:52:37 -0400

    numbers are variables

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 name = '"'
-print("%sProgramming is like building a multilingual puzzle" % name)
+print("%sProgramming is like building a multilingual puzzle"% name)
--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 number = 98
-print(f'% d Battery street' % number, end='')
+print(f'%d Battery street' % number)
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
