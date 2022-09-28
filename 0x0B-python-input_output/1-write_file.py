f438d0a834a3e19b9b72cb255bacddb188258747
cosmic <kingsleyfrancis42@gmail.com>
2022-09-27 22:44:55 -0400

    not another stressfull ordeal

--- 0x0B-python-input_output/1-write_file.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def write_file(filename="", text=""):
-    with open(filename, mode="w+", encoding="utf-8") as fd:
-        fd.write(text)
-    return len(text)
--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,5 +0,0 @@
-def append_write(filename="", text=""):
-    with open(filename,'a+', encoding='utf-8') as f:
-        f.write(text)
-
-
