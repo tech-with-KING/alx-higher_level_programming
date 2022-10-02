c36300cdc9f90f7378358d8eb509a3e6243e7313
cosmic <kingsleyfrancis42@gmail.com>
2022-10-02 12:52:19 +0100

    made to stand out

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,18 @@
+# #!/usr/bin/python3
+'''Base this class initializes many
+ instance of the same object it is the begining of what i want to do'''
+
+
 class Base:
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if self.id == None:
-            self.id = __nb_objects = 0
+        '''function: __init__() Returns
+        the initialized items
+        '''
+        self.id = Base.__nb_objects
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
         else:
             self.id = id
-            
-        
